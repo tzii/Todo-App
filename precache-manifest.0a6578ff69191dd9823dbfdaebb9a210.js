c9a10b4a7fe0c5e4d775ef0f4452afcc4abdb24d
Tuấn Vũ <tuanvundvn@gmail.com>
2020-09-23 22:47:01 +0700

    Updates

---
+++ precache-manifest.0a6578ff69191dd9823dbfdaebb9a210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3f60f66fd0525a790ab5055214f1a8",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "44b0d43eadf7f65f83c8",
+    "url": "/Todo-App/static/css/main.3e8dd077.chunk.css"
+  },
+  {
+    "revision": "467d58985c6f57da568f",
+    "url": "/Todo-App/static/js/2.e076921d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo-App/static/js/2.e076921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b0d43eadf7f65f83c8",
+    "url": "/Todo-App/static/js/main.50a35beb.chunk.js"
+  },
+  {
+    "revision": "2ce1f4103cf2debb565c",
+    "url": "/Todo-App/static/js/runtime-main.c7ed4187.js"
+  }
+]);
